b01f2ef14a949b06b55429d383a804e4bcec0061
GoponBag <109653685+GoponBag@users.noreply.github.com>
2022-07-20 20:34:31 +0530

    kdjfi
     asche jache

---
+++ read.py
@@ -0,0 +1,20 @@
+
+f = open('arr2D.text','r')
+count = 0
+while(f.readline()):
+    count = count + 1
+print(count)
+f.close()
+f = open('arr2D.text','r')
+arr=[]
+for i in range (count):
+    a=f.readline()
+    subarr=[]
+    if i == 0:
+        n = a
+    else:
+        subarr = list(a.split(" "))
+        arr.append(subarr)
+print(arr)
+
+
