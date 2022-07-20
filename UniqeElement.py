5913e39d60c1dc01b828ad47b2009e05a1e66da9
GoponBag <109653685+GoponBag@users.noreply.github.com>
2022-07-20 18:39:09 +0530

    Add files via upload

---
+++ UniqeElement.py
@@ -0,0 +1,20 @@
+# Given an array of integers, where all elements but one occur twice, find the unique element.
+
+# Example
+# arr = [1,2,3,4,3,2,1]
+# The unique element is 4 .
+# Print Unique element
+# ===========================Hacker Rank Day 1 one Problem======================================
+a = [1, 2, 3, 4, 3, 2, 1, 5, 8, 7, 9, 10, 7]
+
+
+def lonelyinteger(a):
+    # Write your code here
+    l = len(a)
+    arr = []
+    for i in range(l):
+        if (a.count(a[i]) == 1):
+            arr.append(a[i])
+    return arr
+
+print (lonelyinteger(a))
