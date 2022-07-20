914b491a4b251771ebb196eb5edefd7fc2b65b21
GoponBag <goponbag@gmail.com>
2022-07-20 19:57:28 +0530

    This is a new commit for what I originally planned to be amended

--- Django/myapp/url.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .import views
-urlpatterns = [
-    path('helo/',views.say_hello)
-]
--- Django/myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def say_hello(request):
-    return render ( request, 'hello.html',{"name":"saikat"})
-    # return HttpResponse("Helow Saikat")
-# Create your views here.
