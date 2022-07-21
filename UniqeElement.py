9fd7cbc62d17ddff05a28584657e2afc2ca40fdd
GoponBag <109653685+GoponBag@users.noreply.github.com>
2022-07-21 15:56:38 +0530

    Update UniqeElement.py
    olk

--- UniqeElement.py
+++ UniqeElement.py
@@ -3,7 +3,7 @@
 # Example
 # arr = [1,2,3,4,3,2,1]
 # The unique element is 4 .
-# Print Unique element
+# Print Unique element 565
 # ===========================Hacker Rank Day 1 one Problem======================================
 # iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 a = [1, 2, 3, 4, 3, 2, 1, 5, 8, 7, 9, 10, 7]
